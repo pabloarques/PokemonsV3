306452c0edf02f5b1efefda9bae58873816c8621
pabarqlop <pabloarques98@gmail.com>
2022-10-05 13:03:28 +0200

    mostrar imagenes

--- app/src/main/java/com/example/pokemons/PokemonsAdapter.java
+++ app/src/main/java/com/example/pokemons/PokemonsAdapter.java
@@ -1,9 +1,12 @@
 package com.example.pokemons;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -18,9 +21,17 @@ public class PokemonsAdapter extends ArrayAdapter<Pokemon> {
 
         Pokemon pokemon = getItem(position);
 
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.lv_pokemon_row, parent, false);
+        }
 
+        //union del codigo en las views del layout
 
+        TextView txtPoke = convertView.findViewById(R.id.txtPoke);
+        ImageView img_poke = convertView.findViewById(R.id.img_poke);
 
+        txtPoke.setText(pokemon.getNombre());
 
         return convertView;
     }
